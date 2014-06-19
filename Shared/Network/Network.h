a977ef3b645566aaa75c2cb0f6e3131c8a76062f
Nikola <king_hell@abv.bg>
2014-06-19 22:36:15 +0300

    Remove unneeded ePacketType entries

--- Shared/Network/Network.h
+++ Shared/Network/Network.h
@@ -7,9 +7,6 @@ namespace Network
 	enum ePacketType : unsigned char
 	{
 		PACKET_PLAYER_REGISTERED = 0x87,
-		PACKET_RPC,
-		PACKET_TOGGLE_GUI_COMPONENT,
-		PACKET_START_RADIO,
-		PACKET_SET_WAVE_HEIGHT
+		PACKET_RPC
 	};
 }
