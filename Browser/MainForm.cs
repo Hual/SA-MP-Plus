d099143ce67ab46a80c3cede9e0e3bf64eb77f5a
thefatshizms <brennan9723@hotmail.com>
2014-06-20 22:51:11 +0100

    Server browser

---
+++ Browser/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+using MetroFramework.Forms;
+
+namespace MetroFramework.Browser
+{
+    public partial class MainForm : MetroForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(584, 388);
+            this.Name = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
