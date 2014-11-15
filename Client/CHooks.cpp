f1b6150eac982e9a33da1af323aaea742eb3a55d
GtakillerIV <ahmedelnaggar66@yahoo.com.au>
2014-11-16 00:22:08 +0400

    Fixed Exception Problem - OnPlayerDrinkSprunk

--- Client/CHooks.cpp
+++ Client/CHooks.cpp
@@ -56,7 +56,7 @@ void CHooks::InstallJmp()
 
 	CMem::InstallJmp(0x0584770, CJmpProxy::MarkersHook, CJmpProxy::MarkersHookJmpBack, 6);
 	CMem::InstallJmp(0x4EB484, CJmpProxy::RadioHook, CJmpProxy::RadioHookJmpBack, 6);
-	CMem::InstallJmp(0x55C330, CJmpProxy::DrinkSprunkHook, CJmpProxy::DrinkSprungJmpBack, 5);
+	CMem::InstallJmp(0x55C330, CJmpProxy::DrinkSprunkHook, CJmpProxy::DrinkSprungJmpBack, 6);
 
 }
 
